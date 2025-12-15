d4890883769473b2354559961a6c364e4716aeff
luxe.closet.dz <akahlilzitouni@gmail.com>
2025-12-15 16:03:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function updateCartCount(){
+  let c = document.getElementById("cartCount");
+  if(c) c.innerText = cart.length;
+}
+updateCartCount();
+
+function addToCart(name, price){
+  cart.push({name, price});
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
+  alert("تمت الإضافة للسلة");
+}
+
+if(document.getElementById("orderDetails")){
+  let text="", total=0;
+  cart.forEach(i=>{
+    text+=`- ${i.name} : ${i.price} دج\n`;
+    total+=i.price;
+  });
+  document.getElementById("orderDetails").value =
+  text + "\nالمجموع: " + total + " دج";
+}
+
+document.getElementById("orderForm")?.addEventListener("submit",e=>{
+e.preventDefault();
+if(!confirm("تأكيد الطلب؟")) return;
+
+let n=e.target[0].value;
+let p=e.target[1].value;
+let a=e.target[2].value;
+let d=document.getElementById("state").value;
+let o=document.getElementById("orderDetails").value;
+
+let msg=`🛍️ طلب جديد
+الاسم: ${n}
+الهاتف: ${p}
+العنوان: ${a}
+${o}
+توصيل: ${d} دج`;
+
+window.open("https://wa.me/213668086810?text="+encodeURIComponent(msg));
+});
